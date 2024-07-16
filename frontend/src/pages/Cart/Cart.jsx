808f67fff89c01a3ea1ebe9a76facaee95713442
bashar-ghannam <bashar.b.ghannam@gmail.com>
2024-07-16 13:53:38 +0300

    feat: create cart page feature

--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -1,8 +1,77 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import './Cart.css';
+import { StoreContext } from '../../context/StoreContext';
 
 const Cart = () => {
-  return <div>Cart</div>;
+  const { food_list, cartItems, removeFromCart, getTotalCartAmount } =
+    useContext(StoreContext);
+  return (
+    <div className="cart">
+      <div className="cart-items">
+        <div className="cart-items-title">
+          <p>Items</p>
+          <p>Title</p>
+          <p>Price</p>
+          <p>Quantity</p>
+          <p>Total</p>
+          <p>Remove</p>
+        </div>
+        <br />
+        <hr />
+        {food_list.map((item, index) => {
+          if (cartItems[item._id] > 0) {
+            return (
+              <>
+                <div key={index} className="cart-items-title cart-items-item">
+                  <img src={item.image} alt="" />
+                  <p>{item.name}</p>
+                  <p>${item.price}</p>
+                  <p>{cartItems[item._id]}</p>
+                  <p>${cartItems[item._id] * item.price}</p>
+                  <p className="cross" onClick={() => removeFromCart(item._id)}>
+                    X
+                  </p>
+                </div>
+                <hr />
+              </>
+            );
+          }
+        })}
+      </div>
+      <div className="cart-bottom">
+        <div className="cart-total">
+          <h2>Cart Total</h2>
+          <div>
+            <div className="cart-total-details">
+              <p>Subtotal</p>
+              <p>${getTotalCartAmount()}</p>
+            </div>
+            <hr />
+            <div className="cart-total-details">
+              <p>Delivery fee</p>
+              <p>${getTotalCartAmount() ? 2 : 0}</p>
+            </div>
+            <hr />
+            <div className="cart-total-details">
+              <b>Total</b>
+              <b>${getTotalCartAmount() + (getTotalCartAmount() ? 2 : 0)}</b>
+            </div>
+            <hr />
+          </div>
+          <button>Proceed to checkout</button>
+        </div>
+        <div className="cart-promocode">
+          <div>
+            <p>If you have a promo code ,Enter it here </p>
+            <div className="cart-promocode-input">
+              <input type="text" placeholder="promo code" />
+              <button>Submit</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export { Cart };
