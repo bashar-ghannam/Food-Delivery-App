a65c319fc5599e2315d0a8613eb3fc8b80ed0220
bashar-ghannam <bashar.b.ghannam@gmail.com>
2024-07-18 03:14:18 +0300

    feat(api, frontend): fetch food data on frontend from database
    - Added `fetchFoodList` function in StoreContextProvider to fetch food data from the backend API.
    - Updated `StoreContextProvider` to call `fetchFoodList` on initial load.
    - Modified `FoodItem` component to display food items fetched from the database.
    - Updated `Cart` component to display and calculate cart items dynamically based on fetched food data.

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -1,12 +1,13 @@
 import { createContext, useEffect, useState } from 'react';
-import { food_list } from '../assets/assets';
+import axios from 'axios';
 
 export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
+  const url = 'http://localhost:4000';
+  const [food_list, setFood_list] = useState([]);
   const [cartItems, setCartItems] = useState({});
   const [token, setToken] = useState('');
-  const url = 'http://localhost:4000';
 
   const addToCart = (itemId) => {
     if (!cartItems[itemId]) {
@@ -31,6 +32,25 @@ const StoreContextProvider = (props) => {
     return totalAmount;
   };
 
+  const fetchFoodList = async () => {
+    try {
+      const response = await axios.get(`${url}/api/food/list`);
+      setFood_list(response.data.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    async function loadData() {
+      await fetchFoodList();
+      if (localStorage.getItem('token')) {
+        setToken(localStorage.getItem('token'));
+      }
+    }
+    loadData();
+  }, []);
+
   const contextValue = {
     food_list,
     cartItems,
@@ -42,12 +62,6 @@ const StoreContextProvider = (props) => {
     removeFromCart,
     getTotalCartAmount,
   };
-
-  useEffect(() => {
-    if (localStorage.getItem('token')) {
-      setToken(localStorage.getItem('token'));
-    }
-  }, []);
 
   return (
     <StoreContext.Provider value={contextValue}>
--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -4,7 +4,7 @@ import { StoreContext } from '../../context/StoreContext';
 import { useNavigate } from 'react-router-dom';
 
 const Cart = () => {
-  const { food_list, cartItems, removeFromCart, getTotalCartAmount } =
+  const { food_list, cartItems, removeFromCart, getTotalCartAmount, url } =
     useContext(StoreContext);
   const navigate = useNavigate();
   return (
@@ -23,9 +23,9 @@ const Cart = () => {
         {food_list.map((item, index) => {
           if (cartItems[item._id] > 0) {
             return (
-              <>
-                <div key={index} className="cart-items-title cart-items-item">
-                  <img src={item.image} alt="" />
+              <div key={index}>
+                <div className="cart-items-title cart-items-item">
+                  <img src={url + '/images/' + item.image} alt="" />
                   <p>{item.name}</p>
                   <p>${item.price}</p>
                   <p>{cartItems[item._id]}</p>
@@ -35,7 +35,7 @@ const Cart = () => {
                   </p>
                 </div>
                 <hr />
-              </>
+              </div>
             );
           }
         })}
